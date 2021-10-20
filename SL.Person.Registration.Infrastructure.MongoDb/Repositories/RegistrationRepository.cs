dff957cadabb81bcd44e750cff6d59ff27214f88
lrpedroso36 <lrodriguespedroso@hotmail.com>
2021-10-20 01:22:10 -0300

    change repository

--- SL.Person.Registration.Infrastructure.MongoDb/Repositories/RegistrationRepository.cs
+++ SL.Person.Registration.Infrastructure.MongoDb/Repositories/RegistrationRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using SL.Person.Registration.Domain.PersonAggregate;
+using SL.Person.Registration.Domain.RegistrationAggregate;
 using SL.Person.Registration.Domain.Repositories;
 
 namespace SL.Person.Registration.Infrastructure.MongoDb.Repositories
@@ -12,12 +12,12 @@ namespace SL.Person.Registration.Infrastructure.MongoDb.Repositories
         {
         }
 
-        public Task<PersonRegistration> GetPerson(long documentNumber, CancellationToken cancellationToken)
+        public Task<InformationRegistration> Get(long documentNumber, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> Insert(PersonRegistration person, CancellationToken cancellationToken)
+        public Task<bool> Insert(InformationRegistration registration, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
