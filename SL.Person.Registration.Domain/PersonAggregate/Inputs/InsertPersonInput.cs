86e96243cccaaab268b4cc2e55f434176de4b22a
Leandro Rodrigues Pedroso <lrodriguespedroso@hotmail.com>
2025-03-22 15:25:58 -0300

    inputs

---
+++ SL.Person.Registration.Domain/PersonAggregate/Inputs/InsertPersonInput.cs
@@ -0,0 +1,15 @@
+﻿using SL.Person.Registration.Domain.PersonAggregate.Enuns;
+using System;
+using System.Collections.Generic;
+
+namespace SL.Person.Registration.Domain.PersonAggregate.Inputs;
+
+public class InsertPersonInput
+{
+    public List<PersonType> Type { get; set; }
+    public string Name { get; set; }
+    public GenderType Gender { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public long DocumentNumber { get; set; }
+}
+
---
+++ SL.Person.Registration.Domain/PersonAggregate/Inputs/UpdatePersonInput.cs
@@ -0,0 +1,18 @@
+﻿using SL.Person.Registration.Domain.PersonAggregate.Enuns;
+using System;
+using System.Collections.Generic;
+
+namespace SL.Person.Registration.Domain.PersonAggregate.Inputs;
+
+public class UpdatePersonInput
+{
+    public Guid Id { get; set; }
+    public List<PersonType> Type { get; set; }
+    public string Name { get; set; }
+    public GenderType Gender { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public long DocumentNumber { get; set; }
+    public List<Interview> Interviews { get; set; }
+    public List<Assignment> Assignments { get; set; }
+    public List<WorkSchedule> WorkSchedules { get; set; }
+}
