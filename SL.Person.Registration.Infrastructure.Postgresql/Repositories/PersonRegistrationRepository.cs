a934d01fcbdf87c14e284d24eef5470e725038ad
Leandro Rodrigues Pedroso <lrodriguespedroso@hotmail.com>
2025-03-23 14:03:04 -0300

    - implementation repository

--- SL.Person.Registration.Infrastructure.Postgresql/Repositories/PersonRegistrationRepository.cs
+++ SL.Person.Registration.Infrastructure.Postgresql/Repositories/PersonRegistrationRepository.cs
@@ -1,47 +1,59 @@
-﻿using SL.Person.Registration.Domain.PersonAggregate;
+﻿using Microsoft.EntityFrameworkCore;
+using SL.Person.Registration.Domain.PersonAggregate;
 using SL.Person.Registration.Domain.Repositories;
+using SL.Person.Registration.Infrastructure.Postgresql.Context;
 
 namespace SL.Person.Registration.Infrastructure.Postgresql.Repositories;
 
-public class PersonRegistrationRepository : IPersonRegistrationRepository
+public class PersonRegistrationRepository(ApplicationDbContext context) : IPersonRegistrationRepository
 {
+    private readonly ApplicationDbContext _context;
+
     public Task<IEnumerable<PersonRegistration>?> GetAsync(Guid? personType, string name, long documentNumber, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public Task<PersonRegistration?> GetByDocumentASync(long documentNumber, CancellationToken cancellationToken)
+    public async Task<PersonRegistration?> GetByDocumentASync(long documentNumber, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _context.PersonRegistrations.FirstOrDefaultAsync(x => x.DocumentNumber == documentNumber &&
+                                                        x.IsExcluded, cancellationToken);
     }
 
-    public Task<PersonRegistration?> GetByDocumentAsync(long documentNumber, Guid personTypeId, CancellationToken cancellationToken)
+    public async Task<PersonRegistration?> GetByDocumentAsync(long documentNumber, Guid personTypeId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _context.PersonRegistrations.FirstOrDefaultAsync(x => x.DocumentNumber == documentNumber &&
+                                                        x.PersonRegistrationPersonTypes.Any(x => x.PersonType.Id == personTypeId) &&
+                                                        x.IsExcluded, cancellationToken);
     }
 
-    public Task<PersonRegistration?> GetByIdAsync(string id, CancellationToken cancellationToken)
+    public async Task<PersonRegistration?> GetByIdAsync(string id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _context.PersonRegistrations.FindAsync(id, cancellationToken);
     }
 
-    public Task<IEnumerable<PersonRegistration>?> GetByNameAsync(string name, CancellationToken cancellationToken)
+    public async Task<IEnumerable<PersonRegistration>?> GetByNameAsync(string name, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _context.PersonRegistrations.Where(x => x.Name.ToLower() == name.ToLower() &&
+                                                             x.IsExcluded).ToListAsync(cancellationToken);
     }
 
-    public Task<IEnumerable<PersonRegistration>?> GetByNameAsync(string name, Guid personTypeId, CancellationToken cancellationToken)
+    public async Task<IEnumerable<PersonRegistration>?> GetByNameAsync(string name, Guid personTypeId, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        return await _context.PersonRegistrations.Where(x => x.Name.ToLower() == name.ToLower() &&
+                                                        x.PersonRegistrationPersonTypes.Any(x => x.PersonType.Id == personTypeId) &&
+                                                        x.IsExcluded).ToListAsync(cancellationToken);
     }
 
-    public Task InsertAsync(PersonRegistration registration, CancellationToken cancellationToken)
+    public async Task InsertAsync(PersonRegistration registration, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _context.Add(registration);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public Task UpdateAsync(PersonRegistration registration, CancellationToken cancellationToken)
+    public async Task UpdateAsync(PersonRegistration registration, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _context.Update(registration);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }
